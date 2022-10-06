dbd7fec3b2621585de944d2ae829b5ba6b639a2e
Jersson Rodriguez <34919724+bekaizen@users.noreply.github.com>
2022-10-06 15:29:22 -0400

    Add files via upload

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../partials/Header';
+
+function SignUp() {
+  return (
+    window.location.replace('http://localhost:3001')
+  );
+}
+
+export default SignUp;
